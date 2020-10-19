e2e7e27952d5252807d4842d31d879c13a494436
Adel Kovačević <kovacevic.adel@gmail.com>
2020-10-19 11:46:15 +0200

    test

---
+++ loadtest-int2.scala
@@ -0,0 +1,27 @@
+package computerdatabase // 1
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._ // 2
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation { // 3
+
+  val httpProtocol = http // 4
+    .baseUrl("https://apnews-int.appspot.com") // 5
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("load-test")
+
+  val scn = scenario("Landing") // 7
+    .exec(http("/") // 8
+      .get("/")) // 9
+
+  setUp(
+    scn.inject(
+      rampUsersPerSec(0) to 200 during (60 seconds), // 6
+      constantUsersPerSec(200) during (15 minutes) randomized, // 5
+    ).protocols(httpProtocol)
+  )
+}

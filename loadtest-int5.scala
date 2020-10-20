6c32350f5bd4b6ef34ebaa03b4587c3e05c77e45
QuicklynX19 <kovacevic.adel@gmail.com>
2020-10-20 12:10:45 +0200

    Update loadtest-int5.scala

--- loadtest-int5.scala
+++ loadtest-int5.scala
@@ -21,7 +21,7 @@ class Titanium extends Simulation { // 3
   setUp(
     scn.inject(
       rampUsersPerSec(0) to 50 during (60 seconds), // 6
-      constantUsersPerSec(100) during (120 seconds) randomized, // 5
+      constantUsersPerSec(50) during (120 seconds) randomized, // 5
     ).protocols(httpProtocol)
   )
 }

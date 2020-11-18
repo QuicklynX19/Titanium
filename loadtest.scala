bf0a4d628bc130e97764c96605a2d89aa81b6858
QuicklynX19 <kovacevic.adel@gmail.com>
2020-11-18 11:51:39 +0100

    Update loadtest.scala

--- loadtest.scala
+++ loadtest.scala
@@ -8,20 +8,20 @@ import io.gatling.http.Predef._
 class Titanium extends Simulation { // 3
 
   val httpProtocol = http // 4
-    .baseUrl("https://apnews-prod.appspot.com") // 5
+    .baseUrl("https://apnews-dev.appspot.com/article/plants-top-news-indonesia-trees-sumatra-2a209d60c42bf0e8fcc6f8ea6daa11c7") // 5
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("load-test")
 
-  val scn = scenario("Landing") // 7
+  val scn = scenario("Article") // 7
     .exec(http("/") // 8
       .get("/")) // 9
 
   setUp(
     scn.inject(
-      rampUsersPerSec(0) to 200 during (3 minute), // 6
-      constantUsersPerSec(200) during (3 minutes) randomized, // 5
+      rampUsersPerSec(0) to 5 during (5 seconds), // 6
+      constantUsersPerSec(5) during (1 minutes) randomized, // 5
     ).protocols(httpProtocol)
   )
 }
